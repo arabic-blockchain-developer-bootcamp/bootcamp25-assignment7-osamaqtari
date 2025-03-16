fd3b122753a7a94729105c06cc4d749cfeecd3cb
osamaqtari <osamaqtari@gmail.com>
2025-03-16 05:40:56 +0300

    implement `Assignment7` contract

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -6,13 +6,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Assignment7 is ERC20 {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor() ERC20("Assignment7 token", "ASI7") {}
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address account, uint256 amount) external {
         // Fill in the logic
         // call _mint function with to and amount params
+        _mint(account, amount);
     }
 }
